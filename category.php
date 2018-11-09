2d0483861e145b3fa3e81f990308891dd3d73f5c
hamza202 <k8668975@gmail.com>
2018-11-09 21:16:55 +0200

    First commit

---
+++ category.php
@@ -0,0 +1,260 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content=""/>
+    <meta name="keywords" content="Social Network, Social Media, Make Friends, Newsfeed, Profile Page"/>
+    <meta name="robots" content="index, follow"/>
+    <title>NationsBook</title>
+
+    <!-- Stylesheets
+    ================================================= -->
+    <?php include "style.php" ?>
+
+
+</head>
+<body>
+
+<!-- Header
+================================================= -->
+<?php include "header.php" ?>
+<!--Header End-->
+<div class="">
+    <div id="page-contents" class="timeline-container">
+
+        <div class="container">
+            <div class="row">
+
+                <!-- Newsfeed Common Side Bar Left
+                ================================================= -->
+
+                <div class="col-md-2 static add-container">
+                    <div class="suggestions sticky-sidebar">
+                        <div class="my-Add">
+                            <img class="img-responsive" src="images/p.png" alt="">
+                        </div>
+                        <div class="my-Add">
+                            <img class="img-responsive" src="images/p.png" alt="">
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-8">
+                    <!-- Friend List
+                    ================================================= -->
+                    <div class="px-15 friend-list city-section">
+                        <div class="cat-container">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="cat-big-title">
+                                    <h3>Test Big Title</h3>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-4 col-sm-6">
+                                <div class="albom-container">
+                                    <a class="d-block" href="#">
+                                    <div class="card-overly"></div>
+                                    <img class="img-responsive" src="images/album/1.jpg" alt="">
+                                    <div class="albom-content">
+                                        <h3>
+                                            test tile
+                                        </h3>
+                                    </div>
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="col-md-4 col-sm-6">
+                                <div class="albom-container">
+                                    <a class="d-block" href="#">
+                                        <div class="card-overly"></div>
+                                        <img class="img-responsive" src="images/album/3.jpg" alt="">
+                                        <div class="albom-content">
+                                            <h3>
+                                                test tile
+                                            </h3>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="col-md-4 col-sm-6">
+                                <div class="albom-container">
+                                    <a class="d-block" href="#">
+                                        <div class="card-overly"></div>
+                                        <img class="img-responsive" src="images/album/5.jpg" alt="">
+                                        <div class="albom-content">
+                                            <h3>
+                                                test tile
+                                            </h3>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="col-md-4 col-sm-6">
+                                <div class="albom-container">
+                                    <a class="d-block" href="#">
+                                        <div class="card-overly"></div>
+                                        <img class="img-responsive" src="images/album/6.jpg" alt="">
+                                        <div class="albom-content">
+                                            <h3>
+                                                test tile
+                                            </h3>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="col-md-4 col-sm-6">
+                                <div class="albom-container">
+                                    <a class="d-block" href="#">
+                                        <div class="card-overly"></div>
+                                        <img class="img-responsive" src="images/album/10.jpg" alt="">
+                                        <div class="albom-content">
+                                            <h3>
+                                                test tile
+                                            </h3>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="col-md-4 col-sm-6">
+                                <div class="albom-container">
+                                    <a class="d-block" href="#">
+                                        <div class="card-overly"></div>
+                                        <img class="img-responsive" src="images/album/9.jpg" alt="">
+                                        <div class="albom-content">
+                                            <h3>
+                                                test tile
+                                            </h3>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        </div>
+                        <div class="cat-container">
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="cat-big-title">
+                                        <h3>Test Big Title</h3>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-4 col-sm-6">
+                                    <div class="albom-container">
+                                        <a class="d-block" href="#">
+                                            <div class="card-overly"></div>
+                                            <img class="img-responsive" src="images/album/1.jpg" alt="">
+                                            <div class="albom-content">
+                                                <h3>
+                                                    test tile
+                                                </h3>
+                                            </div>
+                                        </a>
+                                    </div>
+                                </div>
+                                <div class="col-md-4 col-sm-6">
+                                    <div class="albom-container">
+                                        <a class="d-block" href="#">
+                                            <div class="card-overly"></div>
+                                            <img class="img-responsive" src="images/album/3.jpg" alt="">
+                                            <div class="albom-content">
+                                                <h3>
+                                                    test tile
+                                                </h3>
+                                            </div>
+                                        </a>
+                                    </div>
+                                </div>
+                                <div class="col-md-4 col-sm-6">
+                                    <div class="albom-container">
+                                        <a class="d-block" href="#">
+                                            <div class="card-overly"></div>
+                                            <img class="img-responsive" src="images/album/5.jpg" alt="">
+                                            <div class="albom-content">
+                                                <h3>
+                                                    test tile
+                                                </h3>
+                                            </div>
+                                        </a>
+                                    </div>
+                                </div>
+                                <div class="col-md-4 col-sm-6">
+                                    <div class="albom-container">
+                                        <a class="d-block" href="#">
+                                            <div class="card-overly"></div>
+                                            <img class="img-responsive" src="images/album/6.jpg" alt="">
+                                            <div class="albom-content">
+                                                <h3>
+                                                    test tile
+                                                </h3>
+                                            </div>
+                                        </a>
+                                    </div>
+                                </div>
+                                <div class="col-md-4 col-sm-6">
+                                    <div class="albom-container">
+                                        <a class="d-block" href="#">
+                                            <div class="card-overly"></div>
+                                            <img class="img-responsive" src="images/album/10.jpg" alt="">
+                                            <div class="albom-content">
+                                                <h3>
+                                                    test tile
+                                                </h3>
+                                            </div>
+                                        </a>
+                                    </div>
+                                </div>
+                                <div class="col-md-4 col-sm-6">
+                                    <div class="albom-container">
+                                        <a class="d-block" href="#">
+                                            <div class="card-overly"></div>
+                                            <img class="img-responsive" src="images/album/9.jpg" alt="">
+                                            <div class="albom-content">
+                                                <h3>
+                                                    test tile
+                                                </h3>
+                                            </div>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <?php include 'sidebar.php' ?>
+                <!-- Newsfeed Common Side Bar Right
+                ================================================= -->
+
+            </div>
+        </div>
+    </div>
+</div>
+
+<div>
+
+
+</div>
+<!--preloader-->
+<div id="spinner-wrapper">
+    <div class="spinner"></div>
+</div>
+
+
+<!-- Scripts
+================================================= -->
+<script src="js/jquery-3.1.1.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/jquery.sticky-kit.min.js"></script>
+<script src="js/jquery.scrollbar.min.js"></script>
+<script src="js/lightslider.min.js"></script>
+<script src="js/lightgallery-all.min.js"></script>
+<script src="js/fileinput.min.js"></script>
+<script src="js/script.js"></script>
+
+</body>
+
+</html>
